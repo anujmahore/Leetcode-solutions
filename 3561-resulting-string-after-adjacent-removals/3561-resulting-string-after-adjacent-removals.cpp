21e57515d303bb08893674a8ba73802fa403c698
anujmahore <anujmahore21@gmail.com>
2026-09-20 20:22:32 +0530

    Time: 49 ms (84.99%), Space: 55.1 MB (82.28%) - LeetHub

--- 3561-resulting-string-after-adjacent-removals/3561-resulting-string-after-adjacent-removals.cpp
+++ 3561-resulting-string-after-adjacent-removals/3561-resulting-string-after-adjacent-removals.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    string resultingString(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(st.size()==0) st.push(s[i]);
-            else{
-                if(st.size()>0 && (abs(s[i]-st.top())==1 || abs(s[i]-st.top())-24==1 )){
-                    st.pop();
-                }
-                else st.push(s[i]);
-            }
+    bool consecutive(char a, char b) {
+        return abs(a - b) == 1 || 
+               (a == 'a' && b == 'z') ||
+               (a == 'z' && b == 'a');
+    }
 
+    string resultingString(string s) {
+        string st;
+
+        for (char c : s) {
+            if (!st.empty() && consecutive(st.back(), c)) {
+                st.pop_back();
+            } else {
+                st.push_back(c);
+            }
         }
-        string ans;
-        while(st.size()>0){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+        return st;
     }
 };
